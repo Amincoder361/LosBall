199795f24c9ab3f621dbefeebb91a3f0834a5e2f
Amincoder361 <clrapple7@gmail.com>
2025-02-09 12:07:44 +0330

    Update and rename script.js to e

--- script.js
+++
@@ -1,5 +0,0 @@
-function hideMenu() {
-    document.querySelectorAll('.background-one, .background-two, .background-three').forEach(item => {
-        item.style.display = 'none'; // مخفی کردن منو
-    });
-}
