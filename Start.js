ec739f0aae9542c94c6708122150c05290a2ea18
Amincoder361 <clrapple7@gmail.com>
2025-02-09 12:06:10 +0330

    Add files via upload

---
+++ Start.js
@@ -0,0 +1,5 @@
+function hideMenu() {
+    document.querySelectorAll('.background-one, .background-two, .background-three').forEach(item => {
+        item.style.display = 'none'; // مخفی کردن منو
+    });
+}
